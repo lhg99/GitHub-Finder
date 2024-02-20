f24a92c10a88ca8553f157b50ad60d9c96843f58
lhg99 <lee991229@naver.com>
2024-02-20 12:50:44 +0900

    make GitHub Finder by HTML, CSS, Javascript

---
+++ script.js
@@ -0,0 +1,60 @@
+const username = document.getElementById('username');
+const profileContents = document.getElementById('profiles');
+const repoContents = document.getElementById('repos');
+
+class User {
+    constructor(name) {
+        this.name = name;
+    }
+
+    async getData() {
+        const name = username.value;
+
+        try {
+            const userData = await fetch(`https://api.github.com/users/${name}`);
+            const userJson = await userData.json();
+
+            const repoData = await fetch(`https://api.github.com/users/${name}/repos`);
+            const repoJson = await repoData.json();
+            
+            profileContents.innerHTML = `
+            <div>
+                <img class="profile-img" src="${userJson.avatar_url}" alt="user-picture">
+                <button class="profile-btn">View Profile</button>
+            </div>
+            <div class="details">
+                <span class="badge bg-primary">Public Repos: ${repoJson.html_url}</span>
+                <span class="badge bg-secondary">Public Gists: ${userJson.gist_urls}</span>
+                <span class="badge bg-success">Followers: ${userJson.followers}</span>
+                <span class="badge bg-info text-dark">Following: ${userJson.following}</span>
+                <div class="detail-info">
+                    <p>Company: ${userJson.company}</p><hr>
+                    <p>Website/Blog: ${userJson.blog}</p><hr>
+                    <p>Location: ${userJson.location}</p><hr>
+                    <p>Member Since: ${userJson.created_at}</p>
+                </div>
+            </div>`;
+
+            repoJson.forEach(repo => {
+                repoContents.innerHTML += `
+                <div class="repo">
+                    <p>${repo.name}</p>
+                    <div class="repo-detail">
+                        <span class="badge bg-primary">Stars: ${repo.stargazers_count}</span>
+                        <span class="badge bg-secondary">Watchers: ${repo.watchers_count}</span>
+                        <span id="repo-span" class="badge bg-success">Forks: ${repo.forks}</span>
+                    </div>
+                </div>`;
+            })
+        } catch(error) {
+            console.error(error);
+        }
+    }
+};
+
+username.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter' || event.key === '\n') {
+        const user = new User(username.value);
+        user.getData();
+    }
+})
